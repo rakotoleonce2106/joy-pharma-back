ff0271bf60e39f12d278deca92ad5347172771f2
rakotoleonce <jeanleonce@newmanstech.com>
2025-05-28 20:59:45 +0300

    remove metronic

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use App\Controller\Bootstrap\DefaultLayoutController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends DefaultLayoutController
+        class DashboardController extends AbstractController
 {
     #[Route('/', name:'admin_dashboard')]
     public function index(): Response
     {
-        # Include vendors and javascript files for dashboard widgets
-        $this->theme->addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
-
         return $this->render('pages/dashboards/index.html.twig');
     }
 }
