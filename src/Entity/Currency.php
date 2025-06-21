2a5a1a020991d48be8a4ffd947cf4d866f9b2fe5
rakotoleonce <jeanleonce@newmanstech.com>
2025-06-21 11:53:55 +0300

    feat: Add CurrencyType form class for currency label input

--- src/Entity/Currency.php
+++ src/Entity/Currency.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CurrencyRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: CurrencyRepository::class)]
 class Currency
@@ -11,9 +12,11 @@ class Currency
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['product:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['product:read'])]
     private ?string $label = null;
 
     public function getId(): ?int
---
+++ src/Form/CurrencyType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Currency;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CurrencyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label', TextType::class, [
+                'label' => 'currency.form.label',
+                'attr' => [
+                    'placeholder' => 'currency.form.label_placeholder',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Currency::class,
+        ]);
+    }
+}
