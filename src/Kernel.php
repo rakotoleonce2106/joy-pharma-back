4ac4f4e58ea7b98241aa5164e8567d2bb47566ac
RAKOTOARISOA Jean Léonce <94055176+rakotoleonce2106@users.noreply.github.com>
2025-12-04 19:26:00 +0300

    Remove unused ToastServiceCompilerPass from Kernel class
    - Eliminated the build method and its associated ToastServiceCompilerPass from the Kernel class, streamlining the codebase.
    - This change enhances maintainability by removing unnecessary dependencies and simplifying the kernel structure.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,19 +2,10 @@
 
 namespace App;
 
-use App\DependencyInjection\Compiler\ToastServiceCompilerPass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-
-    protected function build(ContainerBuilder $container): void
-    {
-        parent::build($container); 
-        $container->addCompilerPass(new ToastServiceCompilerPass());
-    }
 }
