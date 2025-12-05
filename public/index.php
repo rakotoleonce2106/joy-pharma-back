b239b6278567824d0e5dd092386a6f1e991095b8
RAKOTOARISOA Jean Léonce <94055176+rakotoleonce2106@users.noreply.github.com>
2025-12-05 16:50:50 +0300

    Remove dotenv-vault support from Dockerfile and related scripts; streamline environment variable loading process.

--- public/index.php
+++ public/index.php
@@ -2,9 +2,6 @@
 
 use App\Kernel;
 
-// Load dotenv-vault before Symfony runtime
-require_once dirname(__DIR__).'/load-dotenv-vault.php';
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
