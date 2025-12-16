2cd588cd362e031473d8c1f3695a1ff8aa6cf0cb
RAKOTOARISOA Jean Léonce <94055176+rakotoleonce2106@users.noreply.github.com>
2025-12-16 14:34:55 +0300

    chore: Refactor bundles configuration to conditionally include development bundles based on class existence for improved flexibility

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,7 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
@@ -14,5 +13,15 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     DahRomy\MVola\DahRomyMVolaBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
+
+// Bundles de développement - seulement si les classes existent
+if (class_exists(Symfony\Bundle\MakerBundle\MakerBundle::class)) {
+    $bundles[Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+if (class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
+    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'test' => true];
+}
+
+return $bundles;
